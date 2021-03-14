ca0ae634b6e789c03292e38f50c7a44edf948e37
Daniel Brenners <danielbrenners@daniels-mbp-2.lan>
2021-03-13 20:35:35 -0500

    store point data in obj

--- watercolor.js
+++ watercolor.js
@@ -9,37 +9,43 @@ const sketch = () => {
   return ({ context, width, height }) => {
     const getPolygonPoints = (n, x, y, size) => {
       let points = [];
-      for (edge = 0; edge < n; edge++) {
-        let u = x + size * Math.cos((edge * 2 * Math.PI) / n);
-        let v = y + size * Math.sin((edge * 2 * Math.PI) / n);
-        points.push([u, v]);
+      for (ptIndex = 0; ptIndex < n; ptIndex++) {
+        let u = x + size * Math.cos((ptIndex * 2 * Math.PI) / n);
+        let v = y + size * Math.sin((ptIndex * 2 * Math.PI) / n);
+        points.push({ position: [u, v] });
       }
       return points;
     };
 
-    const warpPoints = (points) => {
+    const warpPolygonPoints = (originalPoints) => {
       let newPoints = [];
-      points.forEach((point, index) => {
-        let currentPoint = point;
-        let nextPoint =
-          index < points.length - 1 ? points[index + 1] : points[0];
-        let midPoint = [
-          lerp(currentPoint[0], nextPoint[0], 0.5) + 100,
-          lerp(currentPoint[1], nextPoint[1], 0.5) + 10,
-        ];
-        newPoints.push(point);
-        newPoints.push(midPoint);
-      });
+      for (ptIndex = 0; ptIndex < originalPoints.length; ptIndex++) {
+        const currentPoint = originalPoints[ptIndex];
+        const nextPoint =
+          ptIndex < originalPoints.length - 1
+            ? originalPoints[ptIndex + 1]
+            : originalPoints[0];
 
+        const midU =
+          lerp(currentPoint.position[0], nextPoint.position[0], 0.5) + 10;
+        const midV =
+          lerp(currentPoint.position[1], nextPoint.position[1], 0.5) + 100;
+        const midPoint = [midU, midV];
+
+        newPoints.push({
+          position: [currentPoint.position[0], currentPoint.position[1]],
+        });
+        newPoints.push({ position: [midPoint[0], midPoint[1]] });
+      }
       return newPoints;
     };
 
     points = getPolygonPoints(4, width / 2, height / 2, 500);
-    const warpedPoints = warpPoints(points);
+    const warpedPoints = warpPolygonPoints(points);
 
     context.beginPath();
     warpedPoints.forEach((point) => {
-      context.lineTo(point[0], point[1]);
+      context.lineTo(point.position[0], point.position[1]);
     });
 
     context.closePath();
